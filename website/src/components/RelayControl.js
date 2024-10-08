d3a891cb7fa77c1c2a340dafe0e5a3a4e631331d
Duy Thong <duythong.ptit@gmail.com>
2024-10-09 02:10:26 +0700

    Refactor RelayControl component to add width to Switch component

--- website/src/components/RelayControl.js
+++ website/src/components/RelayControl.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch } from 'antd';
 
 const RelayControl = ({ relayState, onToggleRelay }) => (
-    <div className="flex items-center space-x-4 mt-4">
+    <div className="flex items-center space-x-4 mt-4 ">
         <Switch
             checked={relayState === 'ON'}
             onChange={onToggleRelay}
             checkedChildren="BẬT"
             unCheckedChildren="TẮT"
+            className='w-16 '
         />
         <span className="text-xl font-semibold text-gray-700">
             Rơ-le đang {relayState === 'OFF' ? 'TẮT' : 'BẬT'}
