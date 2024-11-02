f3ef00450ef4375687be62d65b9a0f987f1b285d
Duy Thong <duythong.ptit@gmail.com>
2024-11-02 14:15:48 +0700

    Refactor Firebase initialization: Add Realtime Database support and export both database and authentication services

--- website/src/firebase.js
+++ website/src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth'; // Import getAuth to initialize Firebase Authentication
 
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCR98EPOgJvByjRJPsapt15YsaEmHKGTcA",
     authDomain: "esp8266firebase-2f31a.firebaseapp.com",
@@ -11,6 +13,11 @@ const firebaseConfig = {
     appId: "1:261851321671:web:e2318ad58b052a6184e862"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+
+// Initialize Firebase services
+const database = getDatabase(app);
+const auth = getAuth(app); // Initialize Firebase Authentication
+// Export both services
+export { database, auth };
