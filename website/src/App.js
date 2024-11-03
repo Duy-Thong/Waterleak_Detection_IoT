7bc97ff6d1c4b367ba284554369785d27a986757
Duy Thong <duythong.ptit@gmail.com>
2024-11-04 03:43:54 +0700

    Implement protected routes in App component: Add RequireLogin component, enhance routing logic for user authentication, and ensure secure access to specific pages.

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import { UserProvider, useUser } from './contexts/UserContext';
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
@@ -10,8 +10,23 @@ import DeviceHistory from './pages/DeviceHistory';
 import Login from './pages/Login/Login';
 import DeviceDetail from './pages/DeviceDetail/DeviceDetail';
 import WarningPage from './pages/WarningPage/WarningPage';
+import RequireLogin from './components/RequireLogin';
 import './App.css';
-function App() {
+
+// Protected Route Component
+const ProtectedRoute = ({ children }) => {
+  const { userId } = useUser();
+  const location = useLocation();
+
+  if (!userId) {
+    return <RequireLogin returnUrl={location.pathname} />;
+  }
+
+  return children;
+};
+
+// Main App Component
+function AppContent() {
   const { setUserId } = useUser();
 
   useEffect(() => {
@@ -24,19 +39,61 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="*" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path="/device/:deviceId/history" element={<DeviceHistory />} />
-        <Route path="/account-management" element={<AccountManagement />} />
-        <Route path="/manage-devices" element={<ManageDevices />} />
-        <Route path="/device/:deviceId" element={<DeviceDetail />} />
-        <Route path='device/:deviceId/warnings' element= {<WarningPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        {/* Protected Routes */}
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+        <Route path="/device/:deviceId" element={
+          <ProtectedRoute>
+            <DeviceDetail />
+          </ProtectedRoute>
+        } />
+        <Route path="/device/:deviceId/history" element={
+          <ProtectedRoute>
+            <DeviceHistory />
+          </ProtectedRoute>
+        } />
+        <Route path="/account-management" element={
+          <ProtectedRoute>
+            <AccountManagement />
+          </ProtectedRoute>
+        } />
+        <Route path="/manage-devices" element={
+          <ProtectedRoute>
+            <ManageDevices />
+          </ProtectedRoute>
+        } />
+        <Route path="device/:deviceId/warnings" element={
+          <ProtectedRoute>
+            <WarningPage />
+          </ProtectedRoute>
+        } />
+
+        {/* Catch all redirect */}
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </Router>
+  );
+}
+
+// Wrapper with UserProvider
+function App() {
+  return (
+    <UserProvider>
+      <AppContent />
+    </UserProvider>
   );
 }
 
