cd86415547251d07d01efc6e0b8bdaf8157ef680
Duy Thong <duythong.ptit@gmail.com>
2024-11-15 23:13:29 +0700

    Refactor Firebase configuration: Move sensitive data to environment variables and create new firebase.ts file for initialization

--- website/src/firebase.js
+++ website/src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore'; // Import getFirestore to ini
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCR98EPOgJvByjRJPsapt15YsaEmHKGTcA",
-    authDomain: "esp8266firebase-2f31a.firebaseapp.com",
-    databaseURL: "https://esp8266firebase-2f31a-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "esp8266firebase-2f31a",
-    storageBucket: "esp8266firebase-2f31a.appspot.com",
-    messagingSenderId: "261851321671",
-    appId: "1:261851321671:web:e2318ad58b052a6184e862"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
